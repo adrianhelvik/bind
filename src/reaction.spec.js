a5134136299e092337ee73f2b77ffcd24583564b
Adrian Helvik <adrian.helvik@specifique.no>
2019-11-12 21:26:31 +0100

    Improved test harness

--- src/reaction.spec.js
+++ src/reaction.spec.js
@@ -1,6 +1,7 @@
 import observable from './observable.js'
 import reaction from './reaction.js'
 import Binding from './Binding.js'
+import batch from './batch.js'
 
 it('can react to an updated binding', () => {
   const binding = new Binding()
@@ -12,5 +13,21 @@ it('can react to an updated binding', () => {
     binding.accessed()
   })
   binding.updated()
+  expect(updated).toBe(1)
+})
+
+it('can react to updated bindings within a batch', () => {
+  const binding = new Binding()
+  let updated = 0
+  binding.onUpdate(() => {
+    updated += 1
+  })
+  reaction(() => {
+    binding.accessed()
+  })
+  batch(() => {
+    binding.updated()
+    binding.updated()
+  })
   expect(updated).toBe(1)
 })
